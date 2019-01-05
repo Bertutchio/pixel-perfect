5244af6062dde11b83ed41aa29a5f7edc08fe0e2
Paul Rodrigues <paulrodrigues@macgyver.home>
2019-01-05 18:31:57 +0100

    build: :building_construction: add new clean swift project

---
+++ pixel-perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pixel-perfect
+//
+//  Created by Paul Rodrigues on 05/01/2019.
+//  Copyright © 2019 Paul Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
